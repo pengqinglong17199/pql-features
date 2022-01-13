77a11182b9c3f717c2343176996ae57f4e0bd838
hyuga <hyuga0410@gmail.com>
2022-01-13 16:13:41 +0800

    update:LogUtil相关抽取到组件

---
+++ feature-saas-context/src/main/java/kfang/agent/feature/saas/logger/LogModule.java
@@ -0,0 +1,18 @@
+package kfang.agent.feature.saas.logger;
+
+/**
+ * LoggerModule
+ *
+ * @author hyuga
+ * @since 2021/3/25
+ */
+public interface LogModule {
+
+    /**
+     * getDesc
+     *
+     * @return desc
+     */
+    String getDesc();
+
+}
