e26a2f225938843ef1fd6b857ed89b81c386ab3b
hyuga <hyuga0410@gmail.com>
2022-08-11 14:46:24 +0800

    update hyugatool

--- feature-saas-context/src/main/java/kfang/agent/feature/saas/utils/QuickEntryUtil.java
+++ feature-saas-context/src/main/java/kfang/agent/feature/saas/utils/QuickEntryUtil.java
@@ -1,13 +1,16 @@
 package kfang.agent.feature.saas.utils;
 
+import cn.hyugatool.core.constants.HyugaConstants;
 import cn.hyugatool.core.lang.Console;
 import cn.hyugatool.core.lang.ConsoleAnsi;
+import cn.hyugatool.core.object.ObjectUtil;
 import cn.hyugatool.core.string.StringUtil;
 import cn.hyugatool.system.NetworkUtil;
 import kfang.agent.feature.saas.constants.SaasConstants;
 import kfang.infra.common.KfangInfraCommonProperties;
 import kfang.infra.common.spring.SpringBeanPicker;
 import org.fusesource.jansi.Ansi;
+import org.springframework.core.env.Environment;
 
 import javax.management.MalformedObjectNameException;
 
@@ -19,14 +22,27 @@ import javax.management.MalformedObjectNameException;
  */
 public final class QuickEntryUtil {
 
+    private static final String SERVER_PORT = "server.port";
+
     public static void print(String operatorSystem) throws MalformedObjectNameException {
-        operatorSystem = operatorSystem.replaceAll("_", "-").toLowerCase();
+        operatorSystem = operatorSystem.replaceAll(HyugaConstants.UNDERLINE, HyugaConstants.HYPHEN).toLowerCase();
         String deploy = SpringBeanPicker.getBean(KfangInfraCommonProperties.class).getEnv().getDeploy();
         if (!SaasConstants.DEV.equals(deploy)) {
             return;
         }
         final String localIpAddr = NetworkUtil.getLocalIpAddr();
-        final int localPort = NetworkUtil.getLocalPort();
+
+        Integer localPort = NetworkUtil.getLocalPort();
+        if (ObjectUtil.isNull(localPort)) {
+            Environment environment = SpringBeanPicker.getBean(Environment.class);
+            if (ObjectUtil.nonNull(environment)) {
+                String serverPort = environment.getProperty(SERVER_PORT);
+                if (StringUtil.hasText(serverPort)) {
+                    localPort = Integer.parseInt(serverPort);
+                }
+            }
+        }
+
         final String separator = StringUtil.repeat("=", 200);
 
         Console.greenLog(separator);
